f1fdc320a3acc1b85215e1997d1281e812a91a00
Nodu <nodu00719@gmail.com>
2026-02-25 11:27:56 +0900

    Init:

--- backend/insertAdmin.sh
+++ backend/insertAdmin.sh
@@ -14,7 +14,7 @@ mysql \
   -h "$local_mysql_host" \
   -u "$local_mysql_user" \
   -p"$local_mysql_password" \
-  "$local_mysql_db" <<EOF
+  "$local_mysql_db" <<'EOF'
 INSERT INTO tb_admins (id, admin_email, admin_password, created_at)
 SELECT 1, '1',
        '$argon2id$v=19$m=65536,t=3,p=4$HGMMIURIaU1J6T0nJMTYuw$1dbpr6QsMQcpquaD+Ewd/AsLrtxgYBvlKMOK4R+f8Nc',
